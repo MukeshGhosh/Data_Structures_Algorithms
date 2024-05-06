43352cb27d4db51e152562de783771d5d6c27218
Mukesh Ghosh <47267510+MukeshGhosh@users.noreply.github.com>
2024-05-06 17:37:40 +0530

    Create gcdOrHcf.cpp

---
+++ Basics/gcdOrHcf.cpp
@@ -0,0 +1,18 @@
+// https://www.naukri.com/code360/problems/hcf-and-lcm_840448?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf
+
+// You are given two integers 'n', and 'm'. Calculate 'gcd(n,m)', without using library functions.
+// Note: The greatest common divisor (gcd) of two numbers 'n' and 'm' is the largest positive number that divides both 'n' and 'm' without leaving a remainder.
+
+
+int calcGCD(int n, int m){
+    
+    // for(int i = min(n,m); i >= 1; i--){
+    //     if(n % i == 0 && m % i == 0){
+    //         return i;
+    //     }
+    // }
+
+    if(m == 0) return n;
+    return calcGCD(m, n % m);
+
+}
