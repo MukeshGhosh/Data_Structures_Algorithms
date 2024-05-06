131352548aef27dd5965498a34e1aa20156500cf
Mukesh Ghosh <47267510+MukeshGhosh@users.noreply.github.com>
2024-05-06 17:31:49 +0530

    Update reverseBits.cpp

--- Basics/reverseBits.cpp
+++ Basics/reverseBits.cpp
@@ -1,6 +1,9 @@
 // https://www.naukri.com/code360/problems/reverse-bits_2181102?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM
 // Medium
 
+// There is a song concert going to happen in the city. Print the price of each ticket which is equal to the number obtained after reversing the bits of a given
+// 32 bits unsigned integer ‘n’.
+
 long reverseBits(long n) {
     int pow = 31;
     long reverse = 0;
