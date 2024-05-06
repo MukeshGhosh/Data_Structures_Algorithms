d7e322811da7f0f9c6d7a065d13acb23bf31d032
Mukesh Ghosh <47267510+MukeshGhosh@users.noreply.github.com>
2024-05-06 17:47:51 +0530

    Update palindromeNumber.cpp

--- Basics/palindromeNumber.cpp
+++ Basics/palindromeNumber.cpp
@@ -15,3 +15,8 @@ bool palindrome(int n)
     }
     return false;
 }
+
+
+// Time Complexity: O(log10N + 1) where N is the input number. The time complexity is determined by the number of digits in the input integer N. 
+//                  In the worst case when N is a multiple of 10 the number of digits in N is log10 N + 1.
+// Space Complexity: O(1) as only a constant amount of additional memory for the reversed number regardless of size of the input number.
