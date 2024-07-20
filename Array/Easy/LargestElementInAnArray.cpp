48fc4e3c26bc70354d6b3431eddbd88f49ba3ecc
Mukesh Ghosh <47267510+MukeshGhosh@users.noreply.github.com>
2024-07-20 12:52:57 +0530

    Largest element in an array

---
+++ Array/Easy/LargestElementInAnArray.cpp
@@ -0,0 +1,14 @@
+class Solution
+{
+public:
+    int largest(vector<int> &arr, int n)
+    {
+        int minVal = INT_MIN;
+        for(int i = 0; i < n; i++){
+            if(arr[i] > minVal){
+                minVal = arr[i];
+            }
+        }
+        return minVal;
+    }
+};
