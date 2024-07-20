f263cf70e9d0063d2bd7f0034ee474af52a01404
Mukesh Ghosh <47267510+MukeshGhosh@users.noreply.github.com>
2024-07-20 17:29:40 +0530

    Update SecondLargestInAnArray.cpp

--- Array/Easy/SecondLargestInAnArray.cpp
+++ Array/Easy/SecondLargestInAnArray.cpp
@@ -1,3 +1,5 @@
+// https://www.geeksforgeeks.org/problems/second-largest3735/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=second-largest
+
 // Better approach with two iterations
 // Time Complexity: O(N)
 // Space Complexity: O(1)
