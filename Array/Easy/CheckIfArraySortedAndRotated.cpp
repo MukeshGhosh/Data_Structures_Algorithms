41e0601bb1f7e6b6d6436550a732d0ee3c3b13fc
Mukesh Ghosh <47267510+MukeshGhosh@users.noreply.github.com>
2024-07-20 13:47:53 +0530

    Create CheckIfArraySortedAndRotated.cpp

---
+++ Array/Easy/CheckIfArraySortedAndRotated.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool check(vector<int>& arr) {
+        int n = arr.size(), cnt = 0;
+        for(int i = 0; i < n-1; i++){
+            if(arr[i] > arr[i+1]){
+                cnt += 1;
+            }
+        }
+        cout << cnt;
+        if(cnt == 0) return true;
+        if(cnt == 1 && arr[0] >= arr[n-1]) return true;
+        return false;
+    }
+};
+
+// Time Complexity: O(N)
+// Space Complexity: O(1)
