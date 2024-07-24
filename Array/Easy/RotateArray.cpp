4b2c906a7875e394c9e720eb94bcfb738d989ce3
Mukesh Ghosh <47267510+MukeshGhosh@users.noreply.github.com>
2024-07-24 22:01:02 +0530

    Update RotateArray.cpp

--- Array/Easy/RotateArray.cpp
+++ Array/Easy/RotateArray.cpp
@@ -12,9 +12,9 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         k = k % n;
-        reverseHelper(nums, 0, n-k-1);
-        reverseHelper(nums, n-k, n-1);
-        reverseHelper(nums, 0, n-1);
+        reverseRight(nums, 0, n-k-1);
+        reverseRight(nums, n-k, n-1);
+        reverseRight(nums, 0, n-1);
     }
 };
 
@@ -31,9 +31,9 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         k = k % n;
-        reverseHelper(nums, 0, k-1);
-        reverseHelper(nums, k, n-1);
-        reverseHelper(nums, 0, n-1);
+        reverseLeft(nums, 0, k-1);
+        reverseLeft(nums, k, n-1);
+        reverseLeft(nums, 0, n-1);
     }
 };
 
