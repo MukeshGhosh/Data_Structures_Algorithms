c27896de7761ae63d4d42e7d157e9ec04ab1fc3f
Mukesh Ghosh <47267510+MukeshGhosh@users.noreply.github.com>
2024-07-24 23:46:43 +0530

    Create TwoSum.cpp

---
+++ Array/Medium/TwoSum.cpp
@@ -0,0 +1,18 @@
+// Two Sum : Check if a pair with given sum exists in Array
+
+string twoSum(int n, vector<int> &arr, int target) {
+    sort(arr.begin(), arr.end());
+    int left = 0, right = n - 1;
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+        if (sum == target) {
+            return "YES";
+        }
+        else if (sum < target) left++;
+        else right--;
+    }
+    return "NO";
+}
+
+// Time Complexity: O(NlogN)
+// Space Complexity: O(1)
